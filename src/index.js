60458e6d5f8d2bf0ae553e07516d7ecd410cbba2
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-02-15 14:50:43 +0500

    pull

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store/store";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
@@ -21,8 +21,8 @@ import "antd/dist/antd.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
