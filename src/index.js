e7db7d82a24e55d5ca2cdb66cdd6b9307542cb9f
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-02-15 14:51:57 +0500

    pull

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store/store";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
